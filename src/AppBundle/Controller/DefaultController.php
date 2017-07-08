dd0fe534fa130603679df30e6c47a156f62344bf
Emmanuel BORGES <contact@eborges.fr>
2017-07-08 02:40:47 +0200

    configure docker compose

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\DBAL\Connection;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,6 +14,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        /** @var Connection $connection */
+        $connection = $this->getDoctrine()->getConnection();
+        $connection->createQueryBuilder()->select('id,test')->from('test')->execute()->fetchAll(\PDO::FETCH_ASSOC);
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
